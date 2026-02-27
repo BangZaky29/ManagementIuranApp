ad7878f702a1bd4f853c754c97c13ef31342dbab
BangZaky0029 <bangzaky0029@gmail.com>
2026-02-27 20:38:35 +0700

    update

--- src/services/laporanService.ts
+++ src/services/laporanService.ts
@@ -1,5 +1,5 @@
 import { supabase } from '../lib/supabaseConfig';
-import * as FileSystem from 'expo-file-system';
+import * as FileSystem from 'expo-file-system/legacy';
 import { decode } from 'base64-arraybuffer';
 
 export interface Report {
