ecbd59abab4ac8cacfb3c2826286e031fcee5746
BangZaky0029 <bangzaky0029@gmail.com>
2026-03-04 15:52:16 +0700

    update

--- src/constants/FeatureFlags.ts
+++ src/constants/FeatureFlags.ts
@@ -1,3 +1,5 @@
+import { GoTrueAdminApi } from "@supabase/supabase-js";
+
 /**
  * Centralize all feature toggles here.
  * This allows us to easily enable/disable features 
@@ -5,10 +7,10 @@
  */
 export const FeatureFlags = {
     // Set to false as requested for "Under Development" state
-    IS_SOUND_SETTINGS_ENABLED: false,
-    IS_MESSAGE_ENABLED: false,
     IS_OTHERS_ENABLED: false,
+    IS_MESSAGE_ENABLED: false,
     IS_DARK_MODE_ENABLED: false,
-    IS_AUTO_BACKUP_ENABLED: false,
+    IS_SOUND_SETTINGS_ENABLED: false,
+    IS_AUTO_BACKUP_ENABLED: true,
     IS_BACKUP_RESTORE_ENABLED: true,
 };
